410405b19c52cb002808720af93a1e7628d44d55
Leah Roberts <leah.roberts@wework.com>
2018-02-13 21:01:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 board = ["o ","o ","o ","o ","X","o ","o ","o ","o "]
 
 def display_board(board)
-  print "   |   |   "
-  print "-----------"
-  print "   | #{board[4]}  |   "
-  print "-----------"
-  print "   |   |   "
+  print    |   |   
+  print -----------
+  print    | #{board[4]}  |   
+  print -----------
+  print    |   |   
 end 
 
 
